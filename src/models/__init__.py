208257dfb44088e7251fd99c8a89d5eae5b96015
Avishkar Arjan <avishkararjan@example.com>
2026-05-23 20:13:00 +0200

    trendlyne response model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,8 @@
 from .screener import ScreenerResponse
+from .trendlyne import TrendlyneResponse
 
 # Mapping of data source names to their corresponding response models
 SOURCE_MODELS = {
     "screener": ScreenerResponse,
+    "trendlyne": TrendlyneResponse,
 }
